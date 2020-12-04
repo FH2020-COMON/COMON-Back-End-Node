4e0ff13aba8335a339658aa5c0a2075a7bc48825
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-12-05 05:02:06 +0900

    Update Uploads Middleware
    upload 미들웨어 경로 수정

--- src/middleware/uploadFiles.ts
+++ src/middleware/uploadFiles.ts
@@ -4,7 +4,7 @@ import path from "path";
 const upload = multer({
   storage: multer.diskStorage({
     destination(req, file, cb) {
-      cb(null, 'uploads/');
+      cb(null, '../uploads/');
     },
     filename(req, file, cb) {
       const ext = path.extname(file.originalname);
