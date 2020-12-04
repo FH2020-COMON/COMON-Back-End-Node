7de96b922051b2d2392780983e93a55cf513b54d
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-12-04 21:17:31 +0900

    Craete Company Model
    companys table

---
+++ src/models/companyModel/attributes.ts
@@ -0,0 +1,10 @@
+interface CompanyAttributes {
+  companyId: number;
+  companyName: string;
+  ceoName: string;
+  like: number;
+}
+
+export {
+  CompanyAttributes
+}
---
+++ src/models/companyModel/companyFactory.ts
@@ -0,0 +1,31 @@
+import Sequelize from "sequelize";
+import { SequelizeAttributes } from "../defaultInterfaceAttributes/sequelize.attributes";
+import { CompanyAttributes } from "./attributes";
+import { CompanyInterface } from "../defaultInterfaceAttributes/model.interfaces";
+
+const CompanyFactory = (sequelize: Sequelize.Sequelize): Sequelize.ModelCtor<CompanyInterface> => {
+  const attributes: SequelizeAttributes<CompanyAttributes> = {
+    companyId: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    companyName: {
+      type: Sequelize.STRING(100),
+      allowNull: false,
+    },
+    ceoName: {
+      type: Sequelize.STRING(50),
+      allowNull: false,
+    },
+    like: {
+      type: Sequelize.BIGINT,
+      allowNull: false,
+    }
+  };
+  const Company = sequelize.define<CompanyInterface, CompanyAttributes>("companys", attributes);
+  return Company;
+}
+
+export default CompanyFactory;
