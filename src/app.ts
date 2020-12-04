ec5911507571e4b083e2b877c5de02f391442d7d
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-12-05 04:58:48 +0900

    Update app.ts
    Router 설정과 모듈 설정

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 
 import { db } from "./models";
+import ComonRouter from "./routes";
+import { request } from "http";
 
 const app: express.Application = express();
 
@@ -16,11 +18,12 @@ dotenv.config({ path: path.join(__dirname, "../.env")});
 app.set("port", process.env.PORT || "3000");
 app.set("view engine", "html");
 
-db.sequelize.sync({ force: false })
+db.sequelize.sync({ force: true })
 .then(() => console.log("Database connection successful"))
 .catch(console.error);  
 
 app.use(morgan("combined"));
+app.use("/", express.static(path.join(__dirname, "../uploads")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -29,11 +32,11 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }));  
-
 app.use(cors());
 
-app.get("/", (req, res, next) =>{
-  res.send("hello");
+app.use("/", ComonRouter);
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).end();
 });
 
 export default app;
