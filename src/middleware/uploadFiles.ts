98be22ed4a6fa9f696a68cc99cc5d46f538fdbf8
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-12-05 21:43:35 +0900

    Update view

--- src/middleware/uploadFiles.ts
+++ src/middleware/uploadFiles.ts
@@ -4,6 +4,7 @@ import path from "path";
 const upload = multer({
   storage: multer.diskStorage({
     destination(req, file, cb) {
+      console.log(file);
       cb(null, '../uploads/');
     },
     filename(req, file, cb) {
