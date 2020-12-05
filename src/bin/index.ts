e9df452634417df1560000b0dde6d251b8e6c6e0
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-12-05 10:58:09 +0900

    Update http

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,4 +1,4 @@
-import https, { ServerOptions, Server } from "https";
+import http, { ServerOptions, Server } from "http";
 import fs from "fs";
 import app from "../app";
 import webSocket from "../socket";
@@ -6,12 +6,7 @@ import verifyToken from "../middleware/VerifyToken";
 
 const port = app.get("port");
 
-const options: ServerOptions = {
-  key: fs.readFileSync("./private.pem"),
-  cert: fs.readFileSync("./public.pem"),
-};
-
-const server: Server = https.createServer(options, app).listen(port, () => {
+const server: Server = http.createServer(app).listen(port, () => {
   console.log("Server on ", app.get("port"));
 });
 
