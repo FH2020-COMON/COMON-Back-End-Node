b7a3e10d27fa2b5f65171dd838353cdb11b045a4
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-12-06 02:49:38 +0900

    Update port

--- src/middleware/VerifyToken.ts
+++ src/middleware/VerifyToken.ts
@@ -12,12 +12,11 @@ const verifyToken: BusinessLogic = async (req, res, next) => {
         message: "Bad Request",
       });
     }
-    const verified = await axios.get(`http://ec2-54-180-98-91.ap-northeast-2.compute.amazonaws.com:8000/auth`, {
+    const verified = await axios.get(`http://ec2-54-180-98-91.ap-northeast-2.compute.amazonaws.com:8080/auth`, {
       headers: {
         authorization: token.slice(7),
       },
     });
-    console.log(verified.data);
     req.email = verified.data;
     next();
   } catch(err) {
